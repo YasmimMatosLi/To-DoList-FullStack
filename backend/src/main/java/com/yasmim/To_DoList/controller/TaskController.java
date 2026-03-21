098dcba82cbe372ae528e740719d63f3d8606207
yasmim <yasmim07matos@gmail.com>
2026-03-21 15:16:41 -0300

    feat: criar controller de Task

---
+++ backend/src/main/java/com/yasmim/To_DoList/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.yasmim.To_DoList.controller;
+
+import com.yasmim.To_DoList.infrastructure.entity.Task;
+import com.yasmim.To_DoList.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin("*")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarTask(@RequestBody Task task){
+        taskService.salvar(task);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<Task> listarTask(){
+        return taskService.listar();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id){
+        taskService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
